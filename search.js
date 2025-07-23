dcba55769b10cf883f7b6e08eb04b447804aa61f
alexzeecomedy <alex@alexzeecomedy.com>
2025-07-23 15:51:56 -0500

    graceful js degredation

--- search.js
+++ search.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let e = document.getElementById("searchForm"),
-    t = document.getElementById("searchInput");
-  e.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let n = t.value.trim();
-    if (n) {
-      let l = encodeURIComponent(n);
-      window.location.href = `/search.html?q=${l}`;
-    }
-  });
-});
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("searchForm"),t=document.getElementById("searchInput");e.addEventListener("submit",e=>{e.preventDefault();let n=t.value.trim();if(n){let a=encodeURIComponent(n);window.location.href=`/search.html?q=${a}`}})});
