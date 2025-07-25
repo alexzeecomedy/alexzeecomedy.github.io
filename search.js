6bc69b6b39e2ba8f0526f7cc03ebf18eb4af7dde
alexzeecomedy <alex@alexzeecomedy.com>
2025-07-25 14:16:37 -0500

    removed search I am a fraud

--- search.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("searchForm"),t=document.getElementById("searchInput");e.addEventListener("submit",e=>{e.preventDefault();let n=t.value.trim();if(n){let a=encodeURIComponent(n);window.location.href=`/search.html?q=${a}`}})});
